86e4a57b0268a01d693baa1f9c0849bf98c25e00
rcp-dev <ryanpufferdev@gmail.com>
2020-02-26 20:27:02 -0600

    styled show page

--- v5/app.js
+++ v5/app.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost/yelp_camp_v4", {
     });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 
