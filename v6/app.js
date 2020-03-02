e0b3fc40ce3caaba21e9aebe598ebb187dfb037b
rcp-dev <ryanpufferdev@gmail.com>
2020-03-01 20:18:23 -0600

    added login functionality

--- v6/app.js
+++ v6/app.js
@@ -160,6 +160,20 @@ app.post("/register", function(req, res){
         });
 });
 
+//show login form
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+app.post("/login", 
+    passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+    
+})
+
 app.listen(3000, function(){
     console.log("YelpCamp Server has started!");
 });
