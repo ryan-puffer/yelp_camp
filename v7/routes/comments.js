b3806ea7a57dbf902a433b8dfc8e2c2674b429c7
rcp-dev <ryanpufferdev@gmail.com>
2020-03-02 06:44:14 -0600

    created routes dir

---
+++ v7/routes/comments.js
@@ -0,0 +1,36 @@
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else {
+            res.render("comments/new", {campground: campground})
+        }
+    })
+    
+});
+
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+    //lookup campground using Id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+
+        }
+    })
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground show page
+
+})
---
+++ v7/routes/index.js
@@ -0,0 +1,49 @@
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register");
+})
+
+//handle sign up logic
+app.post("/register", function(req, res){
+    let newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+        });
+});
+
+//show login form
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+app.post("/login", 
+    passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+    
+});
+
+//logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
