3d2866efe38f22802268a9b1a3437ab25aaf1527
rcp-dev <ryanpufferdev@gmail.com>
2020-03-01 20:47:25 -0600

    added user auth

--- v6/app.js
+++ v6/app.js
@@ -35,7 +35,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
    
-
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
 
 
 app.get("/", function(req, res){
@@ -100,7 +103,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // =========================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -112,7 +115,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using Id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -172,7 +175,20 @@ app.post("/login",
         failureRedirect: "/login"
     }), function(req, res){
     
+});
+
+//logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
 })
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function(){
     console.log("YelpCamp Server has started!");
