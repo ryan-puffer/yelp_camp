7f4078eca2dd1b63e2218ffce0c6b5ee02d6a7f8
rcp-dev <ryanpufferdev@gmail.com>
2020-03-05 21:08:51 -0600

    user comment association

--- v8/app.js
+++ v8/app.js
@@ -22,7 +22,7 @@ mongoose.connect("mongodb://localhost/yelp_camp_v6", {
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+//seedDB(); //seed the database
 
 
 //PASSPORT CONFIGURATION
