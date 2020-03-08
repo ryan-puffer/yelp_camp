d68c9d6306364fc93a9d733b8995ab5bca702b03
rcp-dev <ryanpufferdev@gmail.com>
2020-03-07 20:31:43 -0600

    first commit v10

---
+++ v10/routes/campgrounds.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+
+//index - show all campgrounds
+router.get("/", function(req, res){
+    //get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds:allCampgrounds});
+            
+        }
+    })
+});
+
+//CREATE -- add new campground to db
+router.post("/", isLoggedIn, function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: desc, author: author} //don't really understand this yet
+
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/campgrounds"); //default is to redirect as a get route
+
+        }
+    })//create a new campground and save to db
+    //redirect back to campgrounds page
+});
+
+//NEW -- show form to create new campground
+router.get("/new", isLoggedIn, function(req, res){
+    res.render("campgrounds/new");
+});
+
+
+//SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided id
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+ 
+    //show that campground
+    
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+module.exports = router;
