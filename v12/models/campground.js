71e2193e0ddb4d24d0383a290ed2b79d4a44c57d
rcp-dev <ryanpufferdev@gmail.com>
2020-03-24 13:37:16 -0500

    v12 initial commit

---
+++ v12/models/campground.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+// const Comment = require('./comment');
+// campgroundSchema.pre('remove', async function() {
+// 	await Comment.remove({
+// 		_id: {
+// 			$in: this.comments
+// 		}
+// 	});
+// });
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+
+
+module.exports = mongoose.model("Campground", campgroundSchema);
