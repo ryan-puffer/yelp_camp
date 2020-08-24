926a014633e035d944bb1e937970d7d27e6b782d
ryan-puffer <ryanpufferdev@gmail.com>
2020-08-23 21:15:18 -0500

    add code comments

--- YelpCampDeployed/models/comment.js
+++ YelpCampDeployed/models/comment.js
@@ -1,17 +1,16 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-
+//comment setup
 var commentSchema = mongoose.Schema({
-    text: String,
-    createdAt: { type: Date, default: Date.now},
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    }
+	text: String,
+	createdAt: { type: Date, default: Date.now },
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		username: String
+	}
 });
 
-
-module.exports = mongoose.model("Comment", commentSchema);
+module.exports = mongoose.model('Comment', commentSchema);
